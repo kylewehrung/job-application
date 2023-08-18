b8452d63e8ace0024c7eead09bea3ac0a03d6e73
Kyle Wehrung <kyle.wehrung@gmail.com>
2023-08-17 21:31:55 -0600

    started handling login/signup logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useEffect, useState, useHistory } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignUpForm from "./SignUpForm";
+import Login from "login";
+
 
 function App() {
+  const [user, setUser] = useState(null);
+  const history = useHistory();
+
+
+  useEffect(() => {
+    fetch("/check_session").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    })
+  }, []);
+
+
+  const handleLogin = (user) => {
+    setUser(user);
+    history.push("/application_questions")
+  }
+
+  if (!user) return <Login onLogin={handleLogin} />;
+
+  
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="/" element={<SignUpForm />} />
+          <Route path="/application_questions" element={<SignUpForm />} />
         </Routes>
       </div>
     </BrowserRouter>
