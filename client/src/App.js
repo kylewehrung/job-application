4941b2b1dea87ccc57b58561ce54b0ed808df4cc
Kyle Wehrung <kyle.wehrung@gmail.com>
2023-08-23 00:58:55 -0600

    basic login page, will tweak

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,27 @@
 import React, { useEffect, useState, useHistory } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignUpForm from "./SignUpForm";
-import Login from "login";
-
+import Login from "./Login"; // Updated import
 
 function App() {
   const [user, setUser] = useState(null);
   const history = useHistory();
-
 
   useEffect(() => {
     fetch("/check_session").then((r) => {
       if (r.ok) {
         r.json().then((user) => setUser(user));
       }
-    })
+    });
   }, []);
-
 
   const handleLogin = (user) => {
     setUser(user);
-    history.push("/application_questions")
-  }
+    history.push("/application_questions");
+  };
 
   if (!user) return <Login onLogin={handleLogin} />;
 
-  
   return (
     <BrowserRouter>
       <div className="App">
@@ -38,4 +34,5 @@ function App() {
 }
 
 export default App;
+
 
