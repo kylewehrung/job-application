00bf3e8d88975edff411cb1b2f09d2d35bcc9ce3
Kyle Wehrung <kyle.wehrung@gmail.com>
2023-09-13 19:16:11 -0600

    centered and added color scheme

--- client/src/components/styles/Input.js
+++ client/src/components/styles/Input.js
@@ -1,9 +1,12 @@
 import styled from "styled-components";
 
 const Input = styled.input`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   width: 700px;
   height: 55px; /* Adjust the height as needed */
-  padding: 10px;
+  padding: 12px;
   border: 1px solid #ccc;
   border-radius: 25px;
   font-size: 22px;
